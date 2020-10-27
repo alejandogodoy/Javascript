f16fb7b064576b80bcf7b39cf4ef23890ca19738
Alejandro Godoy <alejandogodoy22@gmail.com>
2020-10-26 22:34:41 -0300

    Variables and Operators

---
+++ index.js
@@ -0,0 +1,19 @@
+// VARIABLES Y OPERADORES
+
+/* EJERCICIO a
+var a = 2;
+var b = 4;
+console.log (a + b);
+*/
+
+/*EJERCICIO b
+var myVar = "Hello world ";
+var myVar2 = "how are you?";
+alert ( myVar += myVar2)
+*/
+
+/*EJERCICIO c
+var myStr = "Hello";
+var myStr2 = "World";
+console.log (myStr.length + myStr2.length) 
+*/
