c0aa63f6aa9441f3c392f8f651aabfe44d32a0da
Alejandro Godoy <alejandogodoy22@gmail.com>
2020-10-26 22:41:30 -0300

    Strings

--- index.js
+++ index.js
@@ -17,3 +17,40 @@ var myStr = "Hello";
 var myStr2 = "World";
 console.log (myStr.length + myStr2.length) 
 */
+
+// STRINGS 
+
+/* EJERCICIO a
+var x = "hello world";
+console.log (x.toUpperCase ());
+*/
+
+/* EJERCICIO b
+var str = "Hello Word";
+var res = str.substring(0, 5);
+console.log (res);
+*/
+
+/* EJERCICIO c
+var str = "Hello World"
+var res = str.substring (8, 11)
+console.log (res);
+*/
+
+/*EJERCICIO d
+var str = "hello world";
+var res = str.substring (0, 1).toUpperCase() + str.substring (1, 11).toLowerCase()
+console.log (res);
+*/
+
+/* EJERCICIO e
+var str = " hello world"
+var res = str.indexOf(" ");
+console.log (res);
+*/
+
+/* EJERCICIO f
+var str = "certificates statements"
+var res = str.substring (0, 1).toUpperCase()+ str.substring (1, 11).toLowerCase() + str.substring(13, 14).toUpperCase() + str.substring (14, 23).toLowerCase()
+console.log (res);
+*/
